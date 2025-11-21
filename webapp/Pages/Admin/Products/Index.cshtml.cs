c5011d522d57c42aaef921697457d3972e96f022
DanielGaudlandJacobsen <djacobsen5@gmail.com>
2025-11-21 22:01:52 +0100

    [Refactor] [Index.cshtml.cs] Fix constructor syntax and initialize mediator correctly

--- webapp/Pages/Admin/Products/Index.cshtml.cs
+++ webapp/Pages/Admin/Products/Index.cshtml.cs
@@ -9,14 +9,34 @@ using TarlBreuJacoBaraKnor.Pages.Admin.Helpers;
 namespace TarlBreuJacoBaraKnor.Pages.Admin.Products;
 [ServiceFilter(typeof(RequireChangingPasswordFilter))]
 [Authorize(Roles = "Admin")]
-public class IndexModel(IMediator mediator) : PageModel
+public class IndexModel : PageModel
 {
-    private readonly IMediator _mediator = mediator;
+    private readonly IMediator _mediator;
 
-    public List<FoodItem> Products { get; private set; } = [];
+    public IndexModel(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public List<FoodItem> Products { get; private set; } = new List<FoodItem>();
 
     public async Task OnGetAsync()
     {
         Products = await _mediator.Send(new Get.Request());
+    }
+
+    public async Task<IActionResult> OnPostDeleteAsync(int id)
+    {
+        try
+        {
+            await _mediator.Send(new Delete.Request(id));
+            TempData["SuccessMessage"] = "Product deleted successfully!";
+        }
+        catch (Exception ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+        }
+
+        return RedirectToPage();
     }
 }
